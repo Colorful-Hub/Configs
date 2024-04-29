ce0dd46081378f3707db5e31fdb03d7fb4eddd9f
Colorful-Hub <131768359+Colorful-Hub@users.noreply.github.com>
2024-04-29 18:55:47 +0700

    Create Blox Fruits Configs.lua

---
+++ Blox Fruits Configs.lua
@@ -0,0 +1,88 @@
+-- Settings Auto Farm
+_G.AutoFarm = {
+    -- Auto Farm Settings
+    ["Auto Farm Level"] = true,
+    ["Hunter Players Quest"] = true,
+    ["Redeem Code At Level"] = 2,
+    ["Auto Farm Factory"] = true,
+    ["Limit Fragment"] = 100000,
+    -- Lever V4
+    ["Auto Pull Lever"] = true,
+    -- If Level Max
+    ["If Level Max Farm Island"] = "Katakuri",
+    -- Boost FPS
+    ["White Screen"] = true,
+    ["Locking FPS"] = 15,
+    -- Race
+    ["Locked Race"] = {"Human","Mink","Fishman"},
+    ["Auto Race V3"] = true,
+    ["Not Go Sea 3 If Not Get Race V3"] = true,
+}
+
+-- Settings Auto Get Weapon
+_G.Weapon = {
+    -- Sword
+    ["Auto Buy Weapon From NPC"] = true,
+    ["Auto Rengoku"] = true,
+    ["Auto Buy Legendary Sword"] = true,
+    ["Auto True Triple Katana"] = true,
+    ["Auto Tushita"] = true,
+    ["Auto Yama"] = true,
+    ["Auto Cursed Dual Katana"] = true,
+    ["Select Weapon Rarity"] = {"Mythical","Legendary"},
+    ["Auto Farm Mastery Sword"] = true,
+    -- Gun
+    ["Auto Soul Guitar"] = true,
+    ["Not Go Sea 3 If Not Get Dark Fragment"] = true,
+}
+
+-- List Boss Name
+_G.Boss = {
+    -- Boss Sea 1
+    "The Saw [Lv. 100] [Boss]",
+    "Greybeard [Lv. 750] [Raid Boss]",
+
+    -- Boss Sea 2
+    "Darkbeard [Lv. 1000] [Raid Boss]",
+    "Cursed Captain [Lv. 1325] [Raid Boss]",
+
+    -- Boss Sea 3
+    "Captain Elephant [Lv. 1875] [Boss]",
+    "Soul Reaper [Lv. 2100] [Raid Boss]",
+    "Dough King [Lv. 2300] [Raid Boss]",
+    "Cake Prince [Lv. 2300] [Raid Boss]",
+    "rip_indra True Form [Lv. 5000] [Raid Boss]",
+    "Beautiful Pirate [Lv. 1950] [Boss]",
+    "Cake Queen [Lv. 2175] [Boss]"
+}
+
+-- Settings Hop Server
+_G.HopServer = {
+    ["Hunter Hop"] = true,
+    ["Find Fruit To Go Sea 3"] = true,
+}
+
+-- Settings Fruits
+_G.Fruits = {
+    ["Select Fruit Sniper [First]"] = {"Dragon-Dragon","Spirit-Spirit","Venom-Venom","Dough-Dough"},
+    ["Select Fruit Sniper [Sub]"] = {"Ice-Ice","Sand-Sand","Dark-Dark","Quake-Quake","Light-Light"},
+    ["Dont Die If Have Fruit"] = {"Dragon-Dragon","Spirit-Spirit","Venom-Venom","Dough-Dough"},
+    ["Eat Fruit in Inventory"] = true,
+    ["Auto Tween To Fruit"] = true
+    ["Start Sniper Fruits"] = true,
+}
+
+-- Webhook Settings
+_G.Webhook = {
+    ["Link Discord Webhook"] = "",
+    ["Start Webhook"] = false,
+    ["Webhook Mode"] = "Send Every .. min",
+    ["Webhook Minute"] = 10,
+    ["Ping User"] = false,
+    ["Send Test Webhook"] = false,
+}
+
+-- Haki Color Settings
+_G.HakiColor = {
+    ["Auto Buy Haki Color"] = true,
+}
